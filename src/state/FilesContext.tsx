7d7ef2ebf33df1ad78c9115d09d7ef489cba03d1
EnFlorian <33962956+EnFlorian@users.noreply.github.com>
2022-06-30 18:20:39 +0200

    Added context for files

---
+++ src/state/FilesContext.tsx
@@ -0,0 +1,48 @@
+import { createContext, useContext, useReducer } from "react";
+import reducer from "./reducers/FilesReducer";
+
+const initialState: IUIState = {
+  isLoading: false,
+  isDarkMode: false,
+  isSidebarOpen: false,
+};
+
+const UIContext = createContext<IUIContext>({
+  ...initialState,
+  setIsLoading: () => {},
+  setIsDarkMode: () => {},
+  setIsSidebarOpen: () => {},
+});
+
+export const UIProvider = ({ children }: IProps) => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const setIsLoading = (isLoading: boolean) => {
+    dispatch({ type: "SET_IS_LOADING", payload: isLoading });
+  };
+
+  const setIsDarkMode = (isDarkMode: boolean) => {
+    dispatch({ type: "SET_IS_DARK_MODE", payload: isDarkMode });
+  };
+
+  const setIsSidebarOpen = (isSidebarOpen: boolean) => {
+    dispatch({ type: "SET_IS_SIDEBAR_OPEN", payload: isSidebarOpen });
+  };
+
+  return (
+    <UIContext.Provider
+      value={{
+        ...state,
+        setIsLoading,
+        setIsDarkMode,
+        setIsSidebarOpen,
+      }}
+    >
+      {children}
+    </UIContext.Provider>
+  );
+};
+
+export const useUIContext = () => {
+  return useContext(UIContext);
+};
